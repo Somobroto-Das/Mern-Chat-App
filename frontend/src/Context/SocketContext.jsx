d65c8149659f6c9ffe2aca75c7cbb74ece061b6a
Somo2905 <soumyabratadas2905@gmail.com>
2024-06-26 17:50:11 +0530

    Socket url updated

--- frontend/src/Context/SocketContext.jsx
+++ frontend/src/Context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5000", {
+			const socket = io("https://chat-app-production-ne3l.onrender.com/", {
 				query: {
 					userId: authUser._id,
 				},
